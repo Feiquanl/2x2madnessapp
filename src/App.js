8d8fbce2d142652f9c2dc4f690b182df2d3ece16
Feiquanl <fliu6@wpi.edu>
2023-10-03 02:20:50 -0400

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
         updateStep()
         if (model.board.isSolved()) {
           console.log('Yay, solved!')
-          updateMessage('Yay, you solved it!')
+          updateMessage('YAY, YOU WIN!')
         }
       } else {
         model.board.selected = group
